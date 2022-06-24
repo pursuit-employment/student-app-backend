08f21c352c5726aaa1020e8f5879afdc69743d82
werner33 <jordanmanley@Pursuits-MacBook-Pro.local>
2022-06-24 12:35:23 -0400

    weekend exercises

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -15,15 +15,17 @@ controller.get('/', (request, response) => {
 
     response.json(studentDataForDelivery);
 
-
-    // //SELECT * FROM students
-    // if(!min && !max){
-    //     // SELECT * FROM students LIMIT $1, [limit]
-    // } else {
-    //     // SELECT * FROM students WHERE id >= $1 AND id <= $2 LIMIT $3, [min, max, limit]
-    // }
-
 });
+
+
+// write a route to get a student by their full name
+
+// implement min and max ids for get students
+
+// write a route to get the grade average of a student by their id
+
+// get all students sorted by their last name
+
 
 // write a route that accepts a student id as part of the path
 // returning an object (JSON), representing the student with that id
